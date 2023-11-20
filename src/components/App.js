0c55c41722d833162c3476c797de44aa64096cfb
sourabh1428 <sppathak1428@gmail.com>
2023-11-20 15:49:58 +0530

    asd

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,29 @@
 
-import React from "react";
+import React, { useEffect, useState } from "react";
 import './../styles/App.css';
 
 const App = () => {
+  const[data,setData]=useState({});
+  
+  function onMouseMove(e){
+    setData(e)
+
+  }
+  
+  useEffect(()=>{
+    window.addEventListener("mousemove",onMouseMove);
+    return () => {
+      window.removeEventListener("mousemove", onMouseMove);
+    };
+  },[]);
+ 
+  
+  
+
   return (
     <div>
         {/* Do not remove the main div */}
+        The mouse Pointer is: {data.x+','+data.y}
     </div>
   )
 }
